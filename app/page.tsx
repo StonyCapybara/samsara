742e5b404d9aa311f660474b7ec22eed7b50c520
StonyCapybara <97652916+StonyCapybara@users.noreply.github.com>
2024-05-08 23:34:51 +0530

    changed apostrophes to &apos;

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
       <div className="bg-[src('/landing.png')]">
         {/* <Image src="/landing.png" width={500} height={400} alt="" /> */}
         <button className="border-white bg-white bg-opacity-10 font-abhaya px-12 py-6">
-          women's collection
+          women&apos;s collection
         </button>
         <button className="border-white bg-white bg-opacity-10 font-abhaya px-12 py-6">
-          men's collection
+          men&apos;s collection
         </button>
       </div>
       <h1 className="font-aero text-7xl text-title text-center">
